f8272f916653e7dacaefac262fb6bc058f42d9c2
flukeyzx <ahadpubgmgamer@gmail.com>
2026-01-19 23:48:18 +0500

    fixed sqlite adapter issue

--- src/main/db.js
+++ src/main/db.js
@@ -1,12 +1,14 @@
 import { PrismaClient } from "../main/generated/prisma/client.ts";
 import { config } from "dotenv";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import Database from "better-sqlite3";
 
 config();
 
-const sqlite = new Database("dev.db");
-const adapter = new PrismaBetterSqlite3(sqlite);
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({
+  url: connectionString,
+});
 
 const globalForPrisma = globalThis;
 
